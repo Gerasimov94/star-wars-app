95ce76e8d2bf1195aafa601884aab21b37ff8e39
Герасимов Алексей <gerasimov.alexey1994@gmail.com>
2023-04-20 00:13:13 +0300

    fix for deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import postcssNesting from 'postcss-nesting';
 import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	css: {
-		postcss: {
-			plugins: [postcssNesting],
-		},
-	},
 	resolve: {
 		alias: {
 			src: path.resolve(__dirname, 'src'),
